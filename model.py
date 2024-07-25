a9ff3dc56475389f60d5c0ab22c7b1b90069b6c4
Sakshi Yadwade-22 <159874331+SakshiYadwade-22@users.noreply.github.com>
2024-07-25 12:48:27 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,19 @@
+# model.py
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+def build_model(input_shape):
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dropout(0.2),
+        Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
